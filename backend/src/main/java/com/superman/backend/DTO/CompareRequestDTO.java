ba53a7385911d51603b1f3b2ddbdcff0c6395afa
ANseunghyeon <ashaaa1106@naver.com>
2023-11-13 03:56:29 +0900

    feat: User의 주소 입력 DB에 연동

--- backend/src/main/java/com/superman/backend/DTO/CompareRequestDTO.java
+++ backend/src/main/java/com/superman/backend/DTO/CompareRequestDTO.java
@@ -1,10 +1,14 @@
 package com.superman.backend.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CompareRequestDTO {
+    @JsonProperty("User")
     private String User;
+    @JsonProperty("HouseNum")
     private int HouseNum;
+    @JsonProperty("HouseAddress")
     private String HouseAddress;
 }
