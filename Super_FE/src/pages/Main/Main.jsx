4e8fde4ee9850edfc6f0bdc08dcf36cfc7e41e35
yunssup <btob031541@gmail.com>
2023-11-16 20:42:45 +0900

    add:main 페이지 전환 설정

--- Super_FE/src/pages/Main/Main.jsx
+++ Super_FE/src/pages/Main/Main.jsx
@@ -1,22 +1,31 @@
-import React, { useState } from "react";
-import { BackGround, Button1, Button2 } from "./Style"; // Assuming you have a Button component in your Style file
+import React from "react";
+import { BackGround, Button1, Button2 } from "./Style";
+import { useNavigate } from "react-router-dom";
 
 function Main() {
+  const navigate = useNavigate();
+
   const handleButtonClick = (buttonType) => {
-    // Handle button click logic based on the buttonType
-    console.log(`Button ${buttonType} clicked`);
+    switch (buttonType) {
+      case "Button1":
+        navigate("/First"); // Navigate to the page you want when Button 1 is clicked
+        break;
+      case "Button2":
+        navigate("/First"); // Navigate to the page you want when Button 2 is clicked (replace "/Second" with the correct path)
+        break;
+      default:
+        break;
+    }
   };
 
   return (
     <BackGround>
-      {/* Button 1 */}
       <Button1 onClick={() => handleButtonClick("Button1")}>
         조건대로
         <br />
         알아보기
       </Button1>
 
-      {/* Button 2 */}
       <Button2 onClick={() => handleButtonClick("Button2")}>
         어느 집이
         <br />
