8f6dba1379ac1a8784391ed60ee18a2ae056765f
yunssup <btob031541@gmail.com>
2023-11-16 20:30:52 +0900

    feat : main.jsx 구현 완성

--- Super_FE/src/pages/Main/Main.jsx
+++ Super_FE/src/pages/Main/Main.jsx
@@ -1,79 +1,29 @@
-//원본
 import React, { useState } from "react";
-import {
-  Title,
-  ButtonGroup,
-  ButtonLive,
-  ButtonMove,
-  BackGround,
-  ButtonGo,
-} from "./Style";
+import { BackGround, Button1, Button2 } from "./Style"; // Assuming you have a Button component in your Style file
 
-function First() {
-  const [liveSelected, setLiveSelected] = useState(false); // 월세 선택 상태
-  const [moveSelected, setMoveSelected] = useState(false); // 대중교통 선택 상태
-
-  const handleLiveClick = () => {
-    setLiveSelected(!liveSelected); // 월세 선택 상태 토글
-    if (liveSelected && moveSelected) {
-      setMoveSelected(false); // 대중교통 선택 상태 해제
-    }
+function Main() {
+  const handleButtonClick = (buttonType) => {
+    // Handle button click logic based on the buttonType
+    console.log(`Button ${buttonType} clicked`);
   };
 
-  const handleMoveClick = () => {
-    setMoveSelected(!moveSelected); // 대중교통 선택 상태 토글
-    if (liveSelected && moveSelected) {
-      setLiveSelected(false); // 월세 선택 상태 해제
-    }
-  };
-  const handleGoClick = () => {
-    if (liveSelected && moveSelected) {
-      //선택 완료 했을 시 다음 페이지로 이동
-    }
-  };
   return (
     <BackGround>
-      <Title>어떻게 살까?</Title>
-      <ButtonGroup>
-        <ButtonLive
-          tabIndex="0"
-          isSelected={liveSelected}
-          onClick={handleLiveClick}
-        >
-          월세
-        </ButtonLive>
-        <ButtonLive
-          tabIndex="0"
-          isSelected={liveSelected}
-          onClick={handleLiveClick}
-        >
-          전세
-        </ButtonLive>
-      </ButtonGroup>
-      <Title>어떻게 다닐까?</Title>
-      <ButtonGroup>
-        <ButtonMove
-          tabIndex="0"
-          isSelected={moveSelected}
-          onClick={handleMoveClick}
-        >
-          대중교통
-        </ButtonMove>
-        <ButtonMove
-          tabIndex="0"
-          isSelected={moveSelected}
-          onClick={handleMoveClick}
-        >
-          자차
-        </ButtonMove>
-      </ButtonGroup>
-      <ButtonGroup>
-        <ButtonGo tabIndex="0" onClick={handleGoClick}>
-          선택 완료
-        </ButtonGo>
-      </ButtonGroup>
+      {/* Button 1 */}
+      <Button1 onClick={() => handleButtonClick("Button1")}>
+        조건대로
+        <br />
+        알아보기
+      </Button1>
+
+      {/* Button 2 */}
+      <Button2 onClick={() => handleButtonClick("Button2")}>
+        어느 집이
+        <br />
+        저렴할까?
+      </Button2>
     </BackGround>
   );
 }
 
-export default First;
+export default Main;
--- Super_FE/src/pages/Main/Style.jsx
+++ Super_FE/src/pages/Main/Style.jsx
@@ -13,80 +13,33 @@ export const BackGround = styled.div`
   justify-content: center;
 `;
 
-export const Title = styled.h1`
-  color: #333;
-  font-size: 36px;
-  margin-bottom: 20px;
-  padding: 10px; /* 텍스트와 텍스트 배경 사이 간격 조절 */
-`;
-
-export const ButtonGroup = styled.div`
-  display: flex;
-  flex-direction: column; /* 요소들을 세로로 배치 */
-  align-items: center; /* 세로 중앙 정렬 */
-  gap: 20px;
-  padding: 10px;
-`;
-
-export const ButtonLive = styled.button`
+export const Button1 = styled.button`
+  /* margin-top: 10%; */
   border-radius: 20px;
   background: rgb(253, 246, 217);
   color: rgb(27, 27, 29);
   font-family: Arimo;
-  font-size: 25px;
+  font-size: 35px;
   font-weight: 700;
-  line-height: 26px;
-  letter-spacing: 0px;
   text-align: center;
   border: none;
   cursor: pointer;
-  width: 201px;
-  height: 59px;
+  width: 300px;
+  height: 250px;
   flex-shrink: 0;
-  outline: none; /* 포커스 시 기본 아웃라인 제거 */
-  &:focus {
-    border: 2px solid #000; /* 포커스 시 테두리 적용 */
-  }
 `;
-
-export const ButtonMove = styled.button`
+export const Button2 = styled.button`
+  margin-top: 10%;
   border-radius: 20px;
   background: rgb(255, 232, 224);
   color: rgb(27, 27, 29);
   font-family: Arimo;
-  font-size: 25px;
+  font-size: 35px;
   font-weight: 700;
-  line-height: 26px;
-  letter-spacing: 0px;
   text-align: center;
   border: none;
   cursor: pointer;
-  width: 201px;
-  height: 59px;
+  width: 300px;
+  height: 250px;
   flex-shrink: 0;
-  outline: none; /* 포커스 시 기본 아웃라인 제거 */
-  &:focus {
-    border: 2px solid #000; /* 포커스 시 테두리 적용 */
-  }
-`;
-export const ButtonGo = styled.button`
-  margin-top: 10%;
-  border-radius: 20px;
-  background: rgb(255, 255, 255);
-  color: rgb(27, 27, 29);
-  font-family: Arimo;
-  font-size: 25px;
-  font-weight: 700;
-  line-height: 26px;
-  letter-spacing: 0px;
-  text-align: center;
-  border: none;
-  cursor: pointer;
-  width: 201px;
-  height: 59px;
-  flex-shrink: 0;
-  outline: none; /* 포커스 시 기본 아웃라인 제거 */
-  &:focus {
-    border: 2px solid #000; /* 포커스 시 테두리 적용 */
-  }
 `;
