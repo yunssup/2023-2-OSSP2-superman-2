7d2ee4777e51741fef7a2c7ed80ead4c9a3b386a
yunssup <btob031541@gmail.com>
2023-11-17 11:32:14 +0900

    feat:compare 구현

--- Super_FE/src/pages/compare/Style.jsx
+++ Super_FE/src/pages/compare/Style.jsx
@@ -18,14 +18,17 @@ export const AddressSearchContainer = styled.div`
   /* margin-bottom: 20px; */
 `;
 
+export const AddressContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 export const UserEnrollText = styled.input`
   width: 50%;
   padding: 8px;
   box-sizing: border-box;
   border: 1px solid #ccc;
-  border-radius: 11px;
   font-size: 14px;
-  /* margin-bottom: 10px; */
 `;
 export const FindButton = styled.button`
   padding: 10px;
@@ -33,10 +36,36 @@ export const FindButton = styled.button`
   background-color: #4caf50;
   color: white;
   border: none;
-  border-radius: 5px;
   font-size: 16px;
 
   &:hover {
     background-color: #45a049;
   }
+`;
+
+const InfoText = styled.div`
+  margin-top: 10px;
+  font-weight: bold;
+`;
+
+export const InsideContainer = styled.div`
+  background-color: rgb(253, 246, 217);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  height: 50%;
+  text-align: center;
+  /* margin-left: 2%; */
+  margin: 2%;
+  flex-direction: column;
+`;
+
+export const RowContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  width: 90%;
+  height: 80%;
+  /* flex-direction: column; */
 `;
