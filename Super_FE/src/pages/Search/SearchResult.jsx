a1f1aa551073a6fb451138634d6fa2d6b4708240
denverahn21@gmail.com <denverahn21@gmail.com>
2023-11-21 00:30:50 +0900

    fix searchresult에 변경사항 적용

--- Super_FE/src/pages/Search/SearchResult.jsx
+++ Super_FE/src/pages/Search/SearchResult.jsx
@@ -17,6 +17,12 @@ function SearchResult(){
         navigate("/Search");
     };
 
+    const [condition, setCondition] = useState('0');
+
+    const handleOption = (e) => {
+        setCondition(e.target.value);
+    }
+
     return (
         <SearchContainer>
             <NavBar>
@@ -26,18 +32,50 @@ function SearchResult(){
                         ←
                     </ButtonReturn>
                 <NavBarRow>
-                    <NavBarSelect>
-                        <option>조건 선택</option>
-                        <option>가격</option>
-                        <option>면적</option>
-                    </NavBarSelect>
+                <NavBarSelect onChange={handleOption}>
+                    <option value='0'>조건 선택</option>
+                    <option value='1'>가격</option>
+                    <option value='2'>면적</option>
+                </NavBarSelect>
+                {condition==='0'?
                     <NavBarSelect>
                         <option>조건 세분화</option>
                     </NavBarSelect>
+                    :null}
+                {condition==='1'?
                     <NavBarSelect>
-                        <option>OO분 이내</option>
+                        <option>조건 세분화</option>
+                        <option>20만원 미만</option>
+                        <option>20 ~ 40만원</option>
+                        <option>40 ~ 60만원</option>
+                        <option>60 ~ 80만원</option>
+                        <option>80 ~ 100만원</option>
+                        <option>100만원 이상</option>
                     </NavBarSelect>
-                </NavBarRow>
+                    :null}
+                {condition==='2'?
+                    <NavBarSelect>
+                        <option>조건 세분화</option>
+                        <option>10평 미만</option>
+                        <option>10 ~ 20평</option>
+                        <option>20 ~ 30평</option>
+                        <option>30 ~ 40평</option>
+                        <option>40 ~ 50평</option>
+                        <option>50 ~ 60평</option>
+                        <option>60평 이상</option>
+                    </NavBarSelect>
+                    :null}
+                <NavBarSelect>
+                    <option>이동 시간 선택</option>
+                    <option>~10분</option>
+                    <option>11분 ~ 20분</option>
+                    <option>21분 ~ 30분</option>
+                    <option>31분 ~ 40분</option>
+                    <option>41분 ~ 50분</option>
+                    <option>51분 ~ 60분</option>
+                    <option>60분 초과</option>
+                </NavBarSelect>
+            </NavBarRow>
             </NavBar>
             <ResultGroup>
                 <ResultPara>
