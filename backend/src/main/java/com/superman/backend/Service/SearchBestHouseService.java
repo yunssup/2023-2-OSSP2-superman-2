7bdc7de86ee9dfbdf2839a4c025ecee3fd781c89
ANseunghyeon <ashaaa1106@naver.com>
2023-12-05 10:49:41 +0900

    feat : crosconfig

--- backend/src/main/java/com/superman/backend/Service/SearchBestHouseService.java
+++ backend/src/main/java/com/superman/backend/Service/SearchBestHouseService.java
@@ -13,7 +13,6 @@ import java.util.List;
 @Service
 public class SearchBestHouseService {
 
-
     private final MonthlyRentRepository monthlyRentRepository;
     private final LumpSumLeaseRepository lumpSumLeaseRepository;
 
@@ -27,16 +26,26 @@ public class SearchBestHouseService {
         int homeType = searchConditionDTO.getHomeType();
 
         if (homeType == 1) {
-            // hometype이 1이면 MonthlyRentData 조회
-            // MonthlyRentData에 대한 조회 로직 추가
-            return monthlyRentRepository.findAll(); // 예시로 모든 MonthlyRentData 반환
+            return getAllMonthlyRentData(searchConditionDTO);
         } else if (homeType == 2) {
-            // hometype이 2이면 LumpSumLeaseData 조회
-            // LumpSumLeaseData에 대한 조회 로직 추가
-            return lumpSumLeaseRepository.findAll(); // 예시로 모든 LumpSumLeaseData 반환
+            return getAllLumpSumLeaseData(searchConditionDTO);
         } else {
-            // 다른 값이 들어올 경우 예외처리 또는 기본 동작 정의
             throw new IllegalArgumentException("Invalid homeType");
         }
     }
+
+    private List<MonthlyRentData> getAllMonthlyRentData(SearchConditionDTO searchConditionDTO) {
+        // MonthlyRentData에 대한 조회 로직 추가
+        // DTO를 사용하여 원하는 기준으로 MonthlyRentData를 조회하는 코드 작성
+        // 예시로 모든 MonthlyRentData 반환
+        return monthlyRentRepository.findAll();
+    }
+
+    private List<LumpSumLeaseData> getAllLumpSumLeaseData(SearchConditionDTO searchConditionDTO) {
+        // LumpSumLeaseData에 대한 조회 로직 추가
+        // DTO를 사용하여 원하는 기준으로 LumpSumLeaseData를 조회하는 코드 작성
+        // 예시로 모든 LumpSumLeaseData 반환
+        return lumpSumLeaseRepository.findAll();
+    }
 }
+
