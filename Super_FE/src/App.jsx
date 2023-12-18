ff8de8b1ec4f7f838a0e39d6dc80417733406c74
yunssup <btob031541@gmail.com>
2023-12-18 16:04:51 +0900

    feat :교통비 가져오기 api 연결 완료

--- Super_FE/src/App.jsx
+++ Super_FE/src/App.jsx
@@ -4,6 +4,8 @@ import Start from "./pages/Start/Start";
 import First from "./pages/First/First";
 import Main from "./pages/Main/Main";
 import Compare from "./pages/compare/compare";
+import CompareResult from "./pages/compare/CompareResult";
+
 import Search from "./pages/Search/Search";
 import SearchResult from "./pages/Search/SearchResult";
 import "./index.css";
@@ -16,6 +18,7 @@ function App() {
         <Route path="/First" element={<First />} />
         <Route path="/Main" element={<Main />} />
         <Route path="/Compare" element={<Compare />} />
+        <Route path="/CompareResult" element={<CompareResult />} />
         <Route path="/Search" element={<Search />} />
         <Route path="/SearchResult" element={<SearchResult />} />
       </Routes>
