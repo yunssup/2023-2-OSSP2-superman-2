acc0e9d6ab07de677719f41f0fff28087c33aa12
denverahn21@gmail.com <denverahn21@gmail.com>
2023-12-18 15:44:15 +0900

    FIX html 변환 처리

--- Super_FE/src/pages/Search/Search.jsx
+++ Super_FE/src/pages/Search/Search.jsx
@@ -62,11 +62,20 @@ function Search() {
     axios.get(`http://localhost:8080/api/region?userid=${userSessionData}&regionid=${parseInt(selectedOptions.select1)}&condition=${parseInt(selectedOptions.select2)}&range=${parseInt(selectedOptions.select3)}&maxtraval=${parseInt(selectedOptions.select4)}`)
       .then(response => {
         const data = response.data;
+        /*
         document.querySelector('#resultGroup :nth-child(1)').innerHTML = data["1"].place;
         document.querySelector('#resultGroup :nth-child(2)').innerHTML = data["2"].place;
         document.querySelector('#resultGroup :nth-child(3)').innerHTML = data["3"].place;
         document.querySelector('#resultGroup :nth-child(4)').innerHTML = data["4"].place;
-        console.log(data);
+        console.log(data);*/
+
+        //배열의 각 객체에 대해 처리
+        data.forEach((item, index) => {
+          const place = item[index + 1].place; // 데이터에서 장소 가져오기
+          const element = document.querySelector(`#resultGroup :nth-child(${index + 1})`);
+          element.innerHTML = place; // DOM에 데이터 삽입
+        });
+
         for(let i = 0;i < result.length;i++) { //투명상태 해제
           result[i].style.display = "block";
         }
@@ -110,7 +119,7 @@ function Search() {
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const select1 = searchParams.get('select1') || '';
-    const select2 = searchParams.get('select2') || '';
+    const select2 = searchParams.get('select2') || '0';
     const select3 = searchParams.get('select3') || '';
     const select4 = searchParams.get('select4') || '';
 
@@ -139,103 +148,103 @@ const handleResultClick = (event) => {
 
   return (
     <BackGround>
-    <SearchContainer>
-      <NavBar>
+      <SearchContainer>
+        <NavBar>
+          <NavBarRow>
+            <NavBarSelect value={selectedOptions.select1} onChange={(e) => handleSelectChange(e, 'select1')}>
+              <option>주소 입력 창 (시/군/구 선택)</option>
+              <option value='11000'>서울특별시 전체</option>
+              <option value='11680'>강남구</option>
+              <option value='11740'>강동구</option>
+              <option value='11305'>강북구</option>
+              <option value='11500'>강서구</option>
+              <option value='11620'>관악구</option>
+              <option value='11215'>광진구</option>
+              <option value='11530'>구로구</option>
+              <option value='11545'>금천구</option>
+              <option value='11350'>노원구</option>
+              <option value='11320'>도봉구</option>
+              <option value='11230'>동대문구</option>
+              <option value='11590'>동작구</option>
+              <option value='11440'>마포구</option>
+              <option value='11410'>서대문구</option>
+              <option value='11650'>서초구</option>
+              <option value='11200'>성동구</option>
+              <option value='11290'>성북구</option>
+              <option value='11710'>송파구</option>
+              <option value='11470'>양천구</option>
+              <option value='11560'>영등포구</option>
+              <option value='11170'>용산구</option>
+              <option value='11380'>은평구</option>
+              <option value='11110'>종로구</option>
+              <option value='11140'>중구</option>
+              <option value='11260'>중랑구</option>
+            </NavBarSelect>
+          </NavBarRow>
+          <NavBarRow>
+            <NavBarSelect value={selectedOptions.select2} onChange={(e) => handleSelectChange(e, 'select2')}>
+              <option value='0'>조건 선택</option>
+              <option value='1'>가격</option>
+              <option value='2'>면적</option>
+            </NavBarSelect>
+            {condition==='0'?
+            <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
+              <option value='0'>조건 세분화</option>
+            </NavBarSelect>
+            :null}
+            {condition==='1'?
+            <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
+              <option value='0'>조건 세분화</option>
+              <option value='1'>20만원 미만</option>
+              <option value='2'>20 ~ 40만원</option>
+              <option value='3'>40 ~ 60만원</option>
+              <option value='4'>60 ~ 80만원</option>
+              <option value='5'>80 ~ 100만원</option>
+              <option value='6'>100만원 이상</option>
+            </NavBarSelect>
+            :null}
+            {condition==='2'?
+            <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
+              <option value='0'>조건 세분화</option>
+              <option value='1'>10평 미만</option>
+              <option value='2'>10 ~ 20평</option>
+              <option value='3'>20 ~ 30평</option>
+              <option value='4'>30 ~ 40평</option>
+              <option value='5'>40 ~ 50평</option>
+              <option value='6'>50 ~ 60평</option>
+              <option value='7'>60평 이상</option>
+            </NavBarSelect>
+            :null}
+            <NavBarSelect value={selectedOptions.select4} onChange={(e) => handleSelectChange(e, 'select4')}>
+              <option value='0'>이동 시간 선택</option>
+              <option value='1'>10분 미만</option>
+              <option value='2'>20분 미만</option>
+              <option value='3'>30분 미만</option>
+              <option value='4'>40분 미만</option>
+              <option value='5'>50분 미만</option>
+              <option value='6'>60분 미만</option>
+              <option value='7'>60분 이상</option>
+            </NavBarSelect>
+          </NavBarRow>
+          <NavBarRow>
+            <NavBarConfirm onClick={handleResultConfirm}>선택 완료</NavBarConfirm>
+          </NavBarRow>
+          <NavBarRow>
+            <NavBarSpan id="span">원하는 동네를 클릭해서 자세한 정보를 확인하세요!</NavBarSpan>
+          </NavBarRow>
+        </NavBar>
+        <ResultGroup id="resultGroup">
+          <ButtonResult className="result" value="1" onClick={handleResultClick}></ButtonResult>
+          <ButtonResult className="result" value="2" onClick={handleResultClick}></ButtonResult>
+          <ButtonResult className="result" value="3" onClick={handleResultClick}></ButtonResult>
+          <ButtonResult className="result" value="4" onClick={handleResultClick}></ButtonResult>
+        </ResultGroup>
         <NavBarRow>
-          <NavBarSelect value={selectedOptions.select1} onChange={(e) => handleSelectChange(e, 'select1')}>
-            <option>주소 입력 창 (시/군/구 선택)</option>
-            <option value='11000'>서울특별시 전체</option>
-            <option value='11680'>강남구</option>
-            <option value='11740'>강동구</option>
-            <option value='11305'>강북구</option>
-            <option value='11500'>강서구</option>
-            <option value='11620'>관악구</option>
-            <option value='11215'>광진구</option>
-            <option value='11530'>구로구</option>
-            <option value='11545'>금천구</option>
-            <option value='11350'>노원구</option>
-            <option value='11320'>도봉구</option>
-            <option value='11230'>동대문구</option>
-            <option value='11590'>동작구</option>
-            <option value='11440'>마포구</option>
-            <option value='11410'>서대문구</option>
-            <option value='11650'>서초구</option>
-            <option value='11200'>성동구</option>
-            <option value='11290'>성북구</option>
-            <option value='11710'>송파구</option>
-            <option value='11470'>양천구</option>
-            <option value='11560'>영등포구</option>
-            <option value='11170'>용산구</option>
-            <option value='11380'>은평구</option>
-            <option value='11110'>종로구</option>
-            <option value='11140'>중구</option>
-            <option value='11260'>중랑구</option>
-          </NavBarSelect>
+          <ResultConfirm
+            onClick={handleConfirmClick}
+          ></ResultConfirm>
         </NavBarRow>
-        <NavBarRow>
-          <NavBarSelect value={selectedOptions.select2} onChange={(e) => handleSelectChange(e, 'select2')}>
-            <option value='0'>조건 선택</option>
-            <option value='1'>가격</option>
-            <option value='2'>면적</option>
-          </NavBarSelect>
-          {condition==='0'?
-          <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
-            <option value='0'>조건 세분화</option>
-          </NavBarSelect>
-          :null}
-          {condition==='1'?
-          <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
-            <option value='0'>조건 세분화</option>
-            <option value='1'>20만원 미만</option>
-            <option value='2'>20 ~ 40만원</option>
-            <option value='3'>40 ~ 60만원</option>
-            <option value='4'>60 ~ 80만원</option>
-            <option value='5'>80 ~ 100만원</option>
-            <option value='6'>100만원 이상</option>
-          </NavBarSelect>
-          :null}
-          {condition==='2'?
-          <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
-            <option value='0'>조건 세분화</option>
-            <option value='1'>10평 미만</option>
-            <option value='2'>10 ~ 20평</option>
-            <option value='3'>20 ~ 30평</option>
-            <option value='4'>30 ~ 40평</option>
-            <option value='5'>40 ~ 50평</option>
-            <option value='6'>50 ~ 60평</option>
-            <option value='7'>60평 이상</option>
-          </NavBarSelect>
-          :null}
-          <NavBarSelect value={selectedOptions.select4} onChange={(e) => handleSelectChange(e, 'select4')}>
-            <option value='0'>이동 시간 선택</option>
-            <option value='1'>10분 미만</option>
-            <option value='2'>20분 미만</option>
-            <option value='3'>30분 미만</option>
-            <option value='4'>40분 미만</option>
-            <option value='5'>50분 미만</option>
-            <option value='6'>60분 미만</option>
-            <option value='7'>60분 이상</option>
-          </NavBarSelect>
-        </NavBarRow>
-        <NavBarRow>
-          <NavBarConfirm onClick={handleResultConfirm}>선택 완료</NavBarConfirm>
-        </NavBarRow>
-        <NavBarRow>
-          <NavBarSpan id="span">원하는 동네를 클릭해서 자세한 정보를 확인하세요!</NavBarSpan>
-        </NavBarRow>
-      </NavBar>
-      <ResultGroup id="resultGroup">
-        <ButtonResult className="result" value="1" onClick={handleResultClick}></ButtonResult>
-        <ButtonResult className="result" value="2" onClick={handleResultClick}></ButtonResult>
-        <ButtonResult className="result" value="3" onClick={handleResultClick}></ButtonResult>
-        <ButtonResult className="result" value="4" onClick={handleResultClick}></ButtonResult>
-      </ResultGroup>
-      <NavBarRow>
-        <ResultConfirm
-          onClick={handleConfirmClick}
-        ></ResultConfirm>
-      </NavBarRow>
-    </SearchContainer>
+      </SearchContainer>
     </BackGround>
   );
 }
--- Super_FE/src/pages/Search/SearchResult.jsx
+++ Super_FE/src/pages/Search/SearchResult.jsx
@@ -84,117 +84,117 @@ function SearchResult(){
 
     return (
         <BackGround>
-        <SearchContainer>
-            <NavBar>
-                <NavBarRow>
-                    <NavBarSelect value={selectedOptions.select1} onChange={(e) => handleSelectChange(e, 'select1')}>
-                        <option>주소 입력 창 (시/군/구 선택)</option>
-                        <option value='11000'>서울특별시 전체</option>
-                        <option value='11680'>강남구</option>
-                        <option value='11740'>강동구</option>
-                        <option value='11305'>강북구</option>
-                        <option value='11500'>강서구</option>
-                        <option value='11620'>관악구</option>
-                        <option value='11215'>광진구</option>
-                        <option value='11530'>구로구</option>
-                        <option value='11545'>금천구</option>
-                        <option value='11350'>노원구</option>
-                        <option value='11320'>도봉구</option>
-                        <option value='11230'>동대문구</option>
-                        <option value='11590'>동작구</option>
-                        <option value='11440'>마포구</option>
-                        <option value='11410'>서대문구</option>
-                        <option value='11650'>서초구</option>
-                        <option value='11200'>성동구</option>
-                        <option value='11290'>성북구</option>
-                        <option value='11710'>송파구</option>
-                        <option value='11470'>양천구</option>
-                        <option value='11560'>영등포구</option>
-                        <option value='11170'>용산구</option>
-                        <option value='11380'>은평구</option>
-                        <option value='11110'>종로구</option>
-                        <option value='11140'>중구</option>
-                        <option value='11260'>중랑구</option>
+            <SearchContainer>
+                <NavBar>
+                    <NavBarRow>
+                        <NavBarSelect value={selectedOptions.select1} onChange={(e) => handleSelectChange(e, 'select1')}>
+                            <option>주소 입력 창 (시/군/구 선택)</option>
+                            <option value='11000'>서울특별시 전체</option>
+                            <option value='11680'>강남구</option>
+                            <option value='11740'>강동구</option>
+                            <option value='11305'>강북구</option>
+                            <option value='11500'>강서구</option>
+                            <option value='11620'>관악구</option>
+                            <option value='11215'>광진구</option>
+                            <option value='11530'>구로구</option>
+                            <option value='11545'>금천구</option>
+                            <option value='11350'>노원구</option>
+                            <option value='11320'>도봉구</option>
+                            <option value='11230'>동대문구</option>
+                            <option value='11590'>동작구</option>
+                            <option value='11440'>마포구</option>
+                            <option value='11410'>서대문구</option>
+                            <option value='11650'>서초구</option>
+                            <option value='11200'>성동구</option>
+                            <option value='11290'>성북구</option>
+                            <option value='11710'>송파구</option>
+                            <option value='11470'>양천구</option>
+                            <option value='11560'>영등포구</option>
+                            <option value='11170'>용산구</option>
+                            <option value='11380'>은평구</option>
+                            <option value='11110'>종로구</option>
+                            <option value='11140'>중구</option>
+                            <option value='11260'>중랑구</option>
+                        </NavBarSelect>
+                    </NavBarRow>
+                    <NavBarRow>
+                    <NavBarSelect value={selectedOptions.select2} onChange={(e) => handleSelectChange(e, 'select2')}>
+                        <option value='0'>조건 선택</option>
+                        <option value='1'>가격</option>
+                        <option value='2'>면적</option>
+                    </NavBarSelect>
+                    {condition==='0'?
+                        <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
+                            <option value='0'>조건 세분화</option>
+                        </NavBarSelect>
+                        :null}
+                    {condition==='1'?
+                        <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
+                            <option value='0'>조건 세분화</option>
+                            <option value='1'>20만원 미만</option>
+                            <option value='2'>20 ~ 40만원</option>
+                            <option value='3'>40 ~ 60만원</option>
+                            <option value='4'>60 ~ 80만원</option>
+                            <option value='5'>80 ~ 100만원</option>
+                            <option value='6'>100만원 이상</option>
+                        </NavBarSelect>
+                        :null}
+                    {condition==='2'?
+                        <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
+                            <option value='0'>조건 세분화</option>
+                            <option value='1'>10평 미만</option>
+                            <option value='2'>10 ~ 20평</option>
+                            <option value='3'>20 ~ 30평</option>
+                            <option value='4'>30 ~ 40평</option>
+                            <option value='5'>40 ~ 50평</option>
+                            <option value='6'>50 ~ 60평</option>
+                            <option value='7'>60평 이상</option>
+                        </NavBarSelect>
+                        :null}
+                    <NavBarSelect value={selectedOptions.select4} onChange={(e) => handleSelectChange(e, 'select4')}>
+                        <option value='0'>이동 시간 선택</option>
+                        <option value='1'>10분 미만</option>
+                        <option value='2'>20분 미만</option>
+                        <option value='3'>30분 미만</option>
+                        <option value='4'>40분 미만</option>
+                        <option value='5'>50분 미만</option>
+                        <option value='6'>60분 미만</option>
+                        <option value='7'>60분 이상</option>
                     </NavBarSelect>
                 </NavBarRow>
+                </NavBar>
+                <ResultGroup>
+                    <ResultPara>
+                        <ResultHeader id='resultPlace'>신내동</ResultHeader>
+                        <ButtonReturn
+                            onClick={handleReturnClick}
+                        ></ButtonReturn>
+                        <ResultDiv>
+                            <ResultSpan>평균 가격</ResultSpan>
+                            <ResultValue id='resultCost'>ABCDEF</ResultValue>
+                        </ResultDiv>
+                        <ResultDiv>
+                            <ResultSpan>평균 면적</ResultSpan>
+                            <ResultValue id='resultArea'>ABCDEF</ResultValue>
+                        </ResultDiv>
+                        <ResultDiv>
+                            <ResultSpan>이동 시간</ResultSpan>
+                            <ResultValue id='resultTime'>ABCDEF</ResultValue>
+                        </ResultDiv>
+                        <ResultDiv>
+                            <ResultSpan>교통비</ResultSpan>
+                            <ResultValue id='resultTransportCost'>ABCDEF</ResultValue>
+                        </ResultDiv>
+                    </ResultPara>
+                </ResultGroup>
                 <NavBarRow>
-                <NavBarSelect value={selectedOptions.select2} onChange={(e) => handleSelectChange(e, 'select2')}>
-                    <option value='0'>조건 선택</option>
-                    <option value='1'>가격</option>
-                    <option value='2'>면적</option>
-                </NavBarSelect>
-                {condition==='0'?
-                    <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
-                        <option value='0'>조건 세분화</option>
-                    </NavBarSelect>
-                    :null}
-                {condition==='1'?
-                    <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
-                        <option value='0'>조건 세분화</option>
-                        <option value='1'>20만원 미만</option>
-                        <option value='2'>20 ~ 40만원</option>
-                        <option value='3'>40 ~ 60만원</option>
-                        <option value='4'>60 ~ 80만원</option>
-                        <option value='5'>80 ~ 100만원</option>
-                        <option value='6'>100만원 이상</option>
-                    </NavBarSelect>
-                    :null}
-                {condition==='2'?
-                    <NavBarSelect value={selectedOptions.select3} onChange={(e) => handleSelectChange(e, 'select3')}>
-                        <option value='0'>조건 세분화</option>
-                        <option value='1'>10평 미만</option>
-                        <option value='2'>10 ~ 20평</option>
-                        <option value='3'>20 ~ 30평</option>
-                        <option value='4'>30 ~ 40평</option>
-                        <option value='5'>40 ~ 50평</option>
-                        <option value='6'>50 ~ 60평</option>
-                        <option value='7'>60평 이상</option>
-                    </NavBarSelect>
-                    :null}
-                <NavBarSelect value={selectedOptions.select4} onChange={(e) => handleSelectChange(e, 'select4')}>
-                    <option value='0'>이동 시간 선택</option>
-                    <option value='1'>10분 미만</option>
-                    <option value='2'>20분 미만</option>
-                    <option value='3'>30분 미만</option>
-                    <option value='4'>40분 미만</option>
-                    <option value='5'>50분 미만</option>
-                    <option value='6'>60분 미만</option>
-                    <option value='7'>60분 이상</option>
-                </NavBarSelect>
-            </NavBarRow>
-            </NavBar>
-            <ResultGroup>
-                <ResultPara>
-                    <ResultHeader id='resultPlace'>신내동</ResultHeader>
-                    <ButtonReturn
-                        onClick={handleReturnClick}
-                    ></ButtonReturn>
-                    <ResultDiv>
-                        <ResultSpan>평균 가격</ResultSpan>
-                        <ResultValue id='resultCost'>ABCDEF</ResultValue>
-                    </ResultDiv>
-                    <ResultDiv>
-                        <ResultSpan>평균 면적</ResultSpan>
-                        <ResultValue id='resultArea'>ABCDEF</ResultValue>
-                    </ResultDiv>
-                    <ResultDiv>
-                        <ResultSpan>이동 시간</ResultSpan>
-                        <ResultValue id='resultTime'>ABCDEF</ResultValue>
-                    </ResultDiv>
-                    <ResultDiv>
-                        <ResultSpan>교통비</ResultSpan>
-                        <ResultValue id='resultTransportCost'>ABCDEF</ResultValue>
-                    </ResultDiv>
-                </ResultPara>
-            </ResultGroup>
-            <NavBarRow>
-                <ResultConfirm
-                    src="public/start/home.png" // Replace this with the actual path or URL of your image
-                    alt="Result Image"
-                    onClick={handleConfirmClick}
-                ></ResultConfirm>
-            </NavBarRow>
-        </SearchContainer>
+                    <ResultConfirm
+                        src="public/start/home.png" // Replace this with the actual path or URL of your image
+                        alt="Result Image"
+                        onClick={handleConfirmClick}
+                    ></ResultConfirm>
+                </NavBarRow>
+            </SearchContainer>
         </BackGround>
     );
 }
